8cd598654ec23006b86e2101d776905412adfc5a
JoseSedeno <jose@kmchealthcare.com>
2025-12-23 08:54:29 +0900

    Update pbs_viewer_app.py

--- pbs_viewer_app.py
+++ pbs_viewer_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 import altair as alt
 import gdown
 import re
-DEBUG_MODE = False  # Turn to True only when you want to see developer diagnostics
+DEBUG_MODE = True  # Turn to True only when you want to see developer diagnostics
 st.set_page_config(page_title="PBS AEMP Viewer", layout="wide")
 st.title("PBS AEMP Price Viewer")
 
